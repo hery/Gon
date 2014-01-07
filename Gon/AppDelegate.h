ebf0c6461d8ba45b7f1a7a623cd8a041415d0ee3
hery <aeonzamaek@gmail.com>
2014-01-07 14:31:25 -0500

    Initial Commit

---
+++ Gon/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Gon
+//
+//  Created by Hery on 1/7/14.
+//  Copyright (c) 2014 Hery Ratsimihah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Gon/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Gon
+//
+//  Created by Hery on 1/7/14.
+//  Copyright (c) 2014 Hery Ratsimihah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
